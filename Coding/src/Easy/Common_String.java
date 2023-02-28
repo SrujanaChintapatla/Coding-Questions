6cf6a8d0b53193378fde00fd55a64880805c185d
SrujanaChintapatla <c.srujana24@gmail.com>
2023-02-28 20:14:05 +0100

    commit

--- Coding/src/Easy/Common_String.java
+++ Coding/src/Easy/Common_String.java
@@ -1,9 +1,28 @@
 package Easy;
 
+import java.util.Arrays;
+
 public class Common_String {
 
 	public static void main(String[] args) {
-		String[] s = {"flower","flow","flight"};
+		String[] s = {"fower","flow","flight"};
+		int c=0;
+		
+		Arrays.sort(s);
+		if(s.length==0 || s.equals(null))
+		{
+			System.out.println("no elements");
+		}
+		while(c<s[0].length())
+		{
+			if(s[0].charAt(c)==s[s.length-1].charAt(c)) {
+				c++;
+			}
+			else {
+			break;}
+		}
+		System.out.println(s[0].substring(0, c));
+		
 	}
 
 }
